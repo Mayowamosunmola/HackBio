b546fac63b3070a6a57db1bd53057dfc076df5d8
Mayowamosunmola <mayowamosunmola@gmail.com>
2025-04-23 23:20:10 +0100

    Create Stage0.py

---
+++ Stage0.py
@@ -0,0 +1,17 @@
+# Stage 0 PROJECT FOR TEAM GLUTAMATE (GAA, GAG)
+TeamName = "Team Glutamate"
+print(f'This is the biodata for {TeamName} in the HackBio AI & Bio coding internship')
+BioData = [
+    ["Name", "Slack Username", "Email", "Hobby", "Country", "Discipline", "Preferred Programming Language"],
+    ["Mayowa Omosanya","Mayowa", "mayowamosunmola@gmail.com","Watching Football", "Scotland","Medical Biotechnology", "Python"],
+    ["John Adams Nyaata", "Adams", "johnadams9644@gmail.com","Car Performance Mod Enthusiast", "Kenya", "Applied Bioengineering", "Python"],
+    ["Noel Idonije", "Noel", "noelemadan@gmail.com", "Watching movies", "Nigeria", "Biochemistry", "Python"]
+]
+# The code below prints out the unique biodata for the members of team Glutamate
+print("                                             ")
+
+print(f"{BioData[0][0]:<20} {BioData[0][1]:<18} {BioData[0][2]:<28} {BioData[0][3]:<31} {BioData[0][4]:<10} {BioData[0][5]:<20} {BioData[0][6]:<10}")
+print("-" * 155)
+print(f"{BioData[1][0]:<20} {BioData[1][1]:<18} {BioData[1][2]:<28} {BioData[1][3]:<31} {BioData[1][4]:<10} {BioData[1][5]:<24} {BioData[1][6]:<10}")
+print(f"{BioData[2][0]:<20} {BioData[2][1]:<18} {BioData[2][2]:<28} {BioData[2][3]:<31} {BioData[2][4]:<10} {BioData[2][5]:<24} {BioData[2][6]:<10}")
+print(f"{BioData[3][0]:<20} {BioData[3][1]:<18} {BioData[3][2]:<28} {BioData[3][3]:<31} {BioData[3][4]:<10} {BioData[3][5]:<24} {BioData[3][6]:<10}")
